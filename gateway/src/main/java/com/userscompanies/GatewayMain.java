78335e442ab3c5ee90071b3b130d60bea7c79770
Marat Tazhetdinov <unlim089@yandex.ru>
2025-06-05 22:43:52 +0300

    Add Spring Cloud Gateway and Eureka

--- gateway/src/main/java/com/userscompanies/GatewayMain.java
+++ gateway/src/main/java/com/userscompanies/GatewayMain.java
@@ -3,13 +3,11 @@ package com.userscompanies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class CompaniesMain {
+public class GatewayMain {
     public static void main(String[] args) {
-        SpringApplication.run(CompaniesMain.class, args);
+        SpringApplication.run(GatewayMain.class, args);
     }
 }
