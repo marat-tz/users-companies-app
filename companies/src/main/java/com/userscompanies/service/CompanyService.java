f00f0306f8ae92fc2174f2095c91b82c214837b8
Marat Tazhetdinov <unlim089@yandex.ru>
2025-06-05 23:35:48 +0300

    Remove unused lines

--- companies/src/main/java/com/userscompanies/service/CompanyService.java
+++ companies/src/main/java/com/userscompanies/service/CompanyService.java
@@ -3,9 +3,6 @@ package com.userscompanies.service;
 import com.userscompanies.dto.CompanyDtoFullResponse;
 import com.userscompanies.dto.CompanyDtoRequest;
 import com.userscompanies.dto.CompanyDtoShortResponse;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
