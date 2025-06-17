c1b6f6b5214bfe3021a4d239941ea01f9aaa7279
Marat Tazhetdinov <unlim089@yandex.ru>
2025-06-17 10:36:34 +0300

    Add loggers in CompaniesController

--- companies/src/main/java/com/userscompanies/controller/CompaniesController.java
+++ companies/src/main/java/com/userscompanies/controller/CompaniesController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/companies")
 @RequiredArgsConstructor
@@ -32,35 +34,40 @@ public class CompaniesController {
 
     @GetMapping("/{companyId}")
     public CompanyDtoFullResponse findCompanyById(@PathVariable Long companyId) {
+        log.info("Получение компании по id = {}", companyId);
         return companyService.findCompanyById(companyId);
     }
 
     @GetMapping
     public List<CompanyDtoFullResponse> findCompanies() {
+        log.info("Получение списка всех компаний");
         return companyService.findCompanies();
     }
 
     @GetMapping("/ids")
     public List<CompanyDtoShortResponse> findCompaniesByIds(@RequestParam(required = false) List<Long> ids) {
+        log.info("Получение компаний по списку id = {}", ids);
         return companyService.findCompaniesByIds(ids);
     }
 
     @PatchMapping("/{companyId}")
     @ResponseStatus(HttpStatus.OK)
     public CompanyDtoShortResponse updateCompanyById(@RequestBody CompanyDtoRequest dto, @PathVariable Long companyId) {
+        log.info("Обновление компании с id = {}, содержимое входящего DTO = {}", companyId, dto);
         return companyService.updateCompanyById(dto, companyId);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CompanyDtoShortResponse createCompany(@Valid @RequestBody CompanyDtoRequest dto) {
+        log.info("Создание новой компании, содержимое входящего DTO = {}", dto);
         return companyService.createCompany(dto);
     }
 
     @DeleteMapping("/{companyId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteCompany(@PathVariable Long companyId) {
+        log.info("Удаление компании с id = {}", companyId);
         companyService.deleteCompany(companyId);
     }
-
 }
