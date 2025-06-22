960de793e1b6f26a30766870d8e3d0156523e939
Marat Tazhetdinov <unlim089@yandex.ru>
2025-06-22 17:24:39 +0300

    Revert "Remove unused imports"
    This reverts commit a4b6100ed8d741336a802299e1fde30a3a7ee0b6.

--- shared/api/src/main/java/com/userscompanies/dto/CompanyDtoRequest.java
+++ shared/api/src/main/java/com/userscompanies/dto/CompanyDtoRequest.java
@@ -1,6 +1,7 @@
 package com.userscompanies.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
--- users/src/main/java/com/userscompanies/client/CompaniesClient.java
+++ users/src/main/java/com/userscompanies/client/CompaniesClient.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @FeignClient(name = "companies", url = "${companies.url}")
 public interface CompaniesClient {
